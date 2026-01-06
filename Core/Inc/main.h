4a417904c6780309924bed7f5d69d65903c47c4b
MateuszTarka00 <80099249+MateuszTarka00@users.noreply.github.com>
2026-01-06 10:02:38 +0100

    Corected Cube pinouts

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -58,16 +58,12 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define IN11_Pin GPIO_PIN_13
-#define IN11_GPIO_Port GPIOC
-#define IN12_Pin GPIO_PIN_14
-#define IN12_GPIO_Port GPIOC
-#define IN8_Pin GPIO_PIN_1
+#define IN9_Pin GPIO_PIN_13
+#define IN9_GPIO_Port GPIOC
+#define IN10_Pin GPIO_PIN_14
+#define IN10_GPIO_Port GPIOC
+#define IN8_Pin GPIO_PIN_3
 #define IN8_GPIO_Port GPIOA
-#define IN9_Pin GPIO_PIN_2
-#define IN9_GPIO_Port GPIOA
-#define IN14_Pin GPIO_PIN_3
-#define IN14_GPIO_Port GPIOA
 #define IN1_Pin GPIO_PIN_4
 #define IN1_GPIO_Port GPIOA
 #define IN3_Pin GPIO_PIN_5
@@ -76,16 +72,10 @@ void Error_Handler(void);
 #define IN2_GPIO_Port GPIOA
 #define IN4_Pin GPIO_PIN_7
 #define IN4_GPIO_Port GPIOA
-#define IN10_Pin GPIO_PIN_1
-#define IN10_GPIO_Port GPIOB
-#define IN13_Pin GPIO_PIN_11
-#define IN13_GPIO_Port GPIOB
-#define OUT17_Pin GPIO_PIN_12
-#define OUT17_GPIO_Port GPIOB
+#define OUT12_Pin GPIO_PIN_12
+#define OUT12_GPIO_Port GPIOB
 #define IN15_Pin GPIO_PIN_13
 #define IN15_GPIO_Port GPIOB
-#define OUT8_Pin GPIO_PIN_14
-#define OUT8_GPIO_Port GPIOB
 #define OUT16_Pin GPIO_PIN_15
 #define OUT16_GPIO_Port GPIOB
 #define OUT15_Pin GPIO_PIN_8
@@ -98,12 +88,12 @@ void Error_Handler(void);
 #define CAN_FAULT_GPIO_Port GPIOA
 #define CAN_OK_Pin GPIO_PIN_3
 #define CAN_OK_GPIO_Port GPIOB
-#define IN16_Pin GPIO_PIN_4
-#define IN16_GPIO_Port GPIOB
-#define OUT7_Pin GPIO_PIN_5
-#define OUT7_GPIO_Port GPIOB
-#define OUT10_Pin GPIO_PIN_8
-#define OUT10_GPIO_Port GPIOB
+#define IN7_Pin GPIO_PIN_4
+#define IN7_GPIO_Port GPIOB
+#define OUT13_Pin GPIO_PIN_5
+#define OUT13_GPIO_Port GPIOB
+#define OUT14_Pin GPIO_PIN_8
+#define OUT14_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -51,41 +51,39 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, OUT17_Pin|OUT8_Pin|OUT16_Pin|CAN_OK_Pin
-                          |OUT7_Pin|OUT10_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, OUT12_Pin|OUT16_Pin|CAN_OK_Pin|OUT13_Pin
+                          |OUT14_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, OUT15_Pin|CAN_FAULT_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : PCPin PCPin */
-  GPIO_InitStruct.Pin = IN11_Pin|IN12_Pin;
+  GPIO_InitStruct.Pin = IN9_Pin|IN10_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PAPin PAPin PAPin PAPin
-                           PAPin PAPin PAPin PAPin
-                           PAPin */
-  GPIO_InitStruct.Pin = IN8_Pin|IN9_Pin|IN14_Pin|IN1_Pin
-                          |IN3_Pin|IN2_Pin|IN4_Pin|IN6_Pin
-                          |IN5_Pin;
+                           PAPin PAPin PAPin */
+  GPIO_InitStruct.Pin = IN8_Pin|IN1_Pin|IN3_Pin|IN2_Pin
+                          |IN4_Pin|IN6_Pin|IN5_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PBPin PBPin PBPin PBPin */
-  GPIO_InitStruct.Pin = IN10_Pin|IN13_Pin|IN15_Pin|IN16_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
   /*Configure GPIO pins : PBPin PBPin PBPin PBPin
-                           PBPin PBPin */
-  GPIO_InitStruct.Pin = OUT17_Pin|OUT8_Pin|OUT16_Pin|CAN_OK_Pin
-                          |OUT7_Pin|OUT10_Pin;
+                           PBPin */
+  GPIO_InitStruct.Pin = OUT12_Pin|OUT16_Pin|CAN_OK_Pin|OUT13_Pin
+                          |OUT14_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PBPin PBPin */
+  GPIO_InitStruct.Pin = IN15_Pin|IN7_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PAPin PAPin */
